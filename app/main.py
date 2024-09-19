5960b5916ad76a0039f97e1eb0d0967a8537c4fd
zefrenchwan <cenalon@yahoo.fr>
2024-09-19 18:48:29 +0200

    linking database

--- app/main.py
+++ app/main.py
@@ -1,30 +1,29 @@
 import os
 from falcon import App
 from psycopg_pool import ConnectionPool
+from resolvers import TagSolver
 from atexit import register
-from resolvers import Resolver
+import logging 
+
+# logging
+logger = logging.getLogger('recueil')
+logger.setLevel(logging.DEBUG)
+log_handler = logging.FileHandler('logs/recueil.log')
+log_handler.setLevel(logging.DEBUG)
+logger.addHandler(log_handler)
 
 # properties settings
-user = os.getenv("USERNAME")
-pwd = os.getenv("PASSWORD")
+db_url = os.getenv("DB_URL")
 # build base objects
-connection=f"postgresql://{user}:{pwd}@localhost:5432/recueil".format(user=user,pwd=pwd)
-pool = ConnectionPool(min_size = 2, max_size = 4, open=False, conninfo=connection)
+pool = ConnectionPool(min_size = 2, max_size = 4, open=True, conninfo=db_url)
 register(pool.close)
 
 # build core objects
-
-class Echo:
-
-    def on_get(self, req, resp):
-        resp.media = "ping"
-
+tagSolver = TagSolver(pool)
 
 # build app
 app = App()
-app.add_route('/echo/', Echo())
-
-#app.add_route('/resolve/{value}/', )
-#app.add_route('/resolve/tag/{tag}/value/{value}/', )
-#app.add_route('/add/value/{value}/as/{tag}', )
+#app.add_route('/check/{value}/', )
+app.add_route('/check/value/{value}/as/{tag}/', tagSolver)
+#app.add_route('/add/value/{value}/as/{tag}/', )
 #app.add_route('/link/child/{child}/to/parent/{parent}/', )
--- app/resolvers.py
+++ app/resolvers.py
@@ -1,36 +1,80 @@
 import json
 import re
+import urllib.parse
 from psycopg.rows import class_row
 from dataclasses import dataclass
 from typing import Optional
+import falcon
 
 
 @dataclass
 class NodeDTO:
+    """
+    General definition of a dto from the database
+    """
     value: str
-    description: Optional[str] = None 
+    description: str 
     tag:str 
     parents: list[str]
 
+    def to_dict(self):
+        """
+        map that value to a dict so it may be returned in a JSON
+        """
+        return self.__dict__
+
 
 class Resolver:
-
+    """
+    Abstract class for all api processing instances 
+    """
     def __init__(self, pool):
+        """
+        Builds a new resolver using a db pool
+        """
         self.pool = pool 
 
-    def normalize(self, value:str) -> str|None:
-        base = re.sub(r'\s\s+', " ")
-        size = len(base)
-        base = base.replace("  ",' ')
-        while size > len(base):
-            size = len(base)
-            base = base.replace("  ",' ')
-        return base.lower()    
+    def normalize(self, value:str) -> str:
+        """
+        Given a string parameter, parses it
+        """
+        return urllib.parse.unquote(value).strip().lower()
 
     def load(self, value:str) -> list[NodeDTO]:
+        """
+        Given a value to search for, get all matching results as node dto
+        """
         result = list()
         with self.pool.connection() as conn:
             with conn.cursor(row_factory=class_row(NodeDTO)) as cursor:
-                for matching in cursor.execute("select * from entities.load_entity(%s)", value):
+                for matching in cursor.execute("select * from entities.load_entity(%s)", [value]):
                     result.append(matching)
         return result
+    
+    
+
+class TagSolver(Resolver):
+    """
+    Falcon interface to look for a value bounded by a tag
+    """
+    def __init__(self, pool):
+        super().__init__(pool)
+
+    def on_get(self, req, resp, value, tag):
+        """
+        Query returns matches for value that is an instance of a given tag (transitive) 
+        """
+        query = self.normalize(value)
+        boundaries = self.normalize(tag)
+        matches = self.load(query)
+        if len(matches) == 0:
+            resp.status = falcon.HTTP_404
+        else:
+            result = []
+            resp.status = falcon.HTTP_200
+            for node in matches:
+                parents = [parent.lower() for parent in node.parents]
+                parents.append(boundaries)
+                if boundaries in parents:
+                    result.append(node.to_dict())
+            resp.media = json.dumps(result)
